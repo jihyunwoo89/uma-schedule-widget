1411994663684553a469508fcfbca24de9e83aa4
Damien Jee <damienjee@MacBook-Air.local>
2026-05-21 14:15:09 +0900

    feat: register all widgets in UmaWidgetBundle

---
+++ Widgets/UmaWidgetBundle.swift
@@ -0,0 +1,19 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct UmaWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LockRectangularWidget()
+        LockCircularWidget()
+        L1MajorDetailWidget()
+        L2AllScheduleWidget()
+        M1MajorBriefWidget()
+        M2ChampionsLoHWidget()
+        M3PickupWidget()
+        S1MajorOverviewWidget()
+        S2ChampionsWidget()
+        S3LoHWidget()
+        S4PickupWidget()
+    }
+}
